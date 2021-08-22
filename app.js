3ad5d22c8fa29c4ece992bbf2456837ef55c28e4
Camilo Jimenez <jucajigue+developer@gmail.com>
2021-08-22 11:08:29 -0500

    Configurar menu para mostrar

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 require('colors')
-const { leerInput } = require('./helpers/inquirer')
+const { inquirerMenu, leerInput, inquirerPausa } = require('./helpers/inquirer')
 
 
 const main = async() => {
 
-    const texto = await leerInput()
-    console.log(texto);
+    let option = null
+
+    do {
+        
+        option = await inquirerMenu()
+
+        console.log({option});
+
+        if(option !== 0 ) await inquirerPausa()
+
+
+    } while (option !== 0);
 }
 
 main()
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -8,31 +8,15 @@ const preguntasMenu = [
         message: 'Que deseas hacer?',
         choices: [
             {
-                value: '1',
-                name: `${'1.'.yellow} Crear una tarea.`
+                value: 1,
+                name: `${'1.'.yellow} Buscar ciudad.`
             },
             {
-                value: '2',
-                name: `${'2.'.yellow} Listar tareas.`
+                value: 2,
+                name: `${'2.'.yellow} Historial.`
             },
             {
-                value: '3',
-                name: `${'3.'.yellow} Listar tareas completadas.`
-            },
-            {
-                value: '4',
-                name: `${'4.'.yellow} Listar tareas pendientes.`
-            },
-            {
-                value: '5',
-                name: `${'5.'.yellow} Completar tarea.`
-            },
-            {
-                value: '6',
-                name: `${'6.'.yellow} Borrar una tarea.`
-            },
-            {
-                value: '0',
+                value: 0,
                 name: `${'0.'.yellow} Salir.`
             },
         ]
