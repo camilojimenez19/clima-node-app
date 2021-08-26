94a86f5b8d04a2197a4268b061b80e8d6c869def
Camilo Jimenez <jucajigue+developer@gmail.com>
2021-08-26 17:59:45 -0500

    Agregar el switch del menu

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 require('colors')
-const { menu, leerInput, pausa } = require('./helpers/inquirer')
+const { menu, leerInput, pausa } = require('./helpers/inquirer');
+const Busquedas = require('./models/Busquedas');
 
 
 const main = async() => {
 
+    const busquedas = new Busquedas()
     let option;
 
+    
     do {
         
         option = await menu()
+
+        switch (option) {
+            case 1:
+                // TODO: Mostrar el mensaje
+                const lugar = await leerInput('Ciudad: ')
+                console.log(lugar);
+
+                // TODO: Buscar los lugares
+
+                // TODO: Seleccionar el lugar
+
+                //TODO: Clima
+
+                //TODO: Mostrar resultados
+
+                console.log('\nInformacion de la ciudad\n'.green);
+                console.log('Ciudad:');
+                console.log('Lat:');
+                console.log('Lng:');
+                console.log('Temperatura:');
+                console.log('Tmp minima:');
+                console.log('Tmp maxima:');
+
+                break;
+            case 2:
+                
+                break;
+        
+            default:
+                break;
+        }
         
 
         if(option !== 0 ) await pausa()
